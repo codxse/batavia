86ce324f18844f414be40dfe4c3c1afbd7349fdf
Nadiar AS <codxse@gmail.com>
2016-07-14 17:06:42 +0700

    ump linebarplus, yet filter not implemented

--- app/pages/upah-minimum-provinsi-line/upah-minimum-provinsi-line.ts
+++ app/pages/upah-minimum-provinsi-line/upah-minimum-provinsi-line.ts
@@ -1,5 +1,10 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, Injectable} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {DataService} from '../../providers/data-service/data-service';
+import {nvD3} from '../../ng2-nvd3';
+import {Options} from '../../providers/options';
+import {Http} from '@angular/http';
+declare let d3: any;
 
 /*
   Generated class for the UpahMinimumProvinsiLinePage page.
@@ -8,8 +13,70 @@ import {NavController} from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
+  directives: [nvD3],
+  providers: [DataService, Options],
   templateUrl: 'build/pages/upah-minimum-provinsi-line/upah-minimum-provinsi-line.html',
 })
 export class UpahMinimumProvinsiLinePage {
-  constructor(public nav: NavController) {}
+  url = "https://api.kawaljakarta.org/v1/ump-inflasi/data&sortBy=tahun&order=asc";
+  data;
+  options;
+  dateArr;
+
+  constructor(public http: Http, public nav: NavController, public dataService: DataService, public getOptions: Options) {
+    console.log("on Constructor");
+  }
+
+  ngOnInit() {
+    console.log("onInit");
+    this.dataService.load(this.url)
+      .then(data => {
+        let keys = ['kenaikan_ump','inflasi','ump'];
+        this.data = this.genData(data, 'tahun', keys, this.genObject);
+        this.options = this.getOptions.loadOptionLinePlus();
+        this.dateArr = this.genKeys('tahun', data);
+        console.log(this.data);
+      });
+  }
+
+  genObject(data, key, dateKey) {
+    let values = [], l = data.length, i;
+    for (i=0; i<l; i++) {
+      if (data[i][key]) {
+        values.push({
+          x: new Date(data[i][dateKey]).getTime(),
+          y: data[i][key]
+        })
+      }
+    }
+    return {
+      key: key,
+      bar: function() {
+        if (key == 'ump') {
+          return true;
+        }
+        return false
+      }(),
+      values: values
+    }
+  }
+
+  genData(data, dateKey, arrKey, genObject) {
+    let arrObj = [];
+    for (let key of arrKey) {
+      arrObj.push(genObject(data, key, 'tahun'))
+    }
+    return arrObj;
+  }
+
+  genKeys(key, arrObj) {
+    let flags = [], cats = [], l = arrObj.length, i;
+    for (i=0; i<l; i++) {
+      if (flags[arrObj[i][key]]) continue;
+      flags[arrObj[i][key]] = true;
+      cats.push(arrObj[i][key]);
+    }
+    return cats;
+  }
+
 }
--- app/providers/options.ts
+++ app/providers/options.ts
@@ -55,6 +55,71 @@ export class Options{
     }
   }
 
+  loadOptionLinePlus() {
+    return {
+      chart: {
+        type: 'linePlusBarChart',
+        height: 400,
+        margin: {
+          top: 5,
+          right: 70,
+          bottom: 15,
+          left: 50
+        },
+        showLabels: true,
+        showLegend: false,
+        bars: {
+          forceY: [0]
+        },
+        legendLeftAxisHint: " (Kiri: Rp)",
+        legendRightAxisHint: " (Kanan: %)",
+        bars2: {
+          forceY: [0]
+        },
+        x: function(d) {
+          return new Date(d.x);
+        },
+        xAxis: {
+          axisLabel: '',
+          tickFormat: function(d) {
+            // var dx = this.data[0].values[d] && this.data[0].values[d].x || 0;
+            // if (dx > 0) {
+              return d3.time.format('%Y')(new Date(d));
+            // }
+            // return null;
+          },
+          showMaxMin: false
+        },
+        x2Axis: {
+          tickFormat: function(d) {
+            // var dx = this.data[0].values[d] && this.data[0].values[d].x || 0;
+            // return d3.time.format('%Y')(new Date(d));
+            return "";
+          },
+          showMaxMin: false
+        },
+        y1Axis: {
+          axisLabel: 'Rp',
+          tickFormat: function(d) {
+            var prefix = d3.formatPrefix(d);
+            return prefix.scale(d) + prefix.symbol;
+          },
+          axisLabelDistance: -10,
+          showMaxMin: false
+        },
+        y2Axis: {
+          axisLabel: '%',
+          tickFormat: function(d) {
+            var prefix = d3.formatPrefix(d);
+            return prefix.scale(d) + prefix.symbol;
+          },
+          showMaxMin: false,
+          axisLabelDistance: -35
+        }
+      }
+    }
+  }
+
   loadOptionPie(title) {
     return {
       chart: {
