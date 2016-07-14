0c33011fa364c6ab463c4cd7c049546f430500ad
Nadiar AS <codxse@gmail.com>
2016-07-14 11:28:30 +0700

    UMP tab page

---
+++ app/pages/upah-minimum-provinsi-tab/upah-minimum-provinsi-tab.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {ViewController, Platform} from 'ionic-angular';
+import {IkhtisarStatistikLinePage} from '../ikhtisar-statistik-line/ikhtisar-statistik-line';
+import {IkhtisarStatistikPiePage} from '../ikhtisar-statistik-pie/ikhtisar-statistik-pie';
+
+@Component({
+  template: `
+    <ion-header>
+      <ion-navbar>
+        <ion-title>Tabs</ion-title>
+      </ion-navbar>
+    </ion-header>
+
+    <ion-content>
+    </ion-content>
+`})
+class TabTextPage {
+  isAndroid: boolean = false;
+
+  constructor(platform: Platform) {
+    this.isAndroid = platform.is('android');
+  }
+}
+
+@Component({
+  template: `
+    <ion-tabs class="tabs-basic">
+      <ion-tab tabTitle="Line" [root]="tabOne"></ion-tab>
+      <ion-tab tabTitle="Scatter" [root]="tabTwo"></ion-tab>
+    </ion-tabs>
+`})
+export class UpahMinimumProvinsiTabPage {
+  tabOne = IkhtisarStatistikLinePage;
+  tabTwo = IkhtisarStatistikPiePage;
+}
