1cbce8cb639283a0a76d7bdf63e28208633154ca
Habib Ridho <habibridho@live.com>
2016-07-15 08:35:55 +0700

    line chart ketenagakerjaan, initial filter implementation

--- app/pages/ikhtisar-statistik-line/ikhtisar-statistik-line.ts
+++ app/pages/ikhtisar-statistik-line/ikhtisar-statistik-line.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter, ViewChild} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {DataService} from '../../providers/data-service/data-service';
 import {nvD3} from '../../ng2-nvd3';
@@ -18,14 +18,18 @@ Ionic pages and navigation.
 })
 
 export class IkhtisarStatistikLinePage{
+  @Output() ngModelChange: EventEmitter<any> = new EventEmitter(false);
+  @ViewChild(nvD3) nvD3: nvD3;
   url = "https://api.kawaljakarta.org/v1/ikhtisar-statistik-antar-kerja/key=tahun&gd='2008'&ld='2014'";
   options;
   data;
+  origData;
   arrObj;
   dateArr;
+  dari: Date;
+  sampai: Date;
 
   constructor(public nav: NavController, public dataService: DataService, public getOptions: Options) {
-    console.log('on constructor main');
     this.arrObj = [];
     this.dataService.load(this.url)
     .then(data => {
@@ -64,15 +68,59 @@ export class IkhtisarStatistikLinePage{
         });
       }
 
+      this.origData = data;
       this.data = genData('rincian', 'jumlah', data, 'tahun');
       this.dateArr = genKeys('tahun', data);
-      console.log(this.dateArr);
+      console.log(this.origData);
     });
   }
 
+  generateData(keyCat, keyValue, arrObj, dateKey, startDate, endDate) {
+    let categories = this.generateKeys(keyCat, arrObj)
+    let parent = this;
+    return categories.map(function(item) {
+      return {
+        key: item,
+        values: parent.generateVals(keyCat, item, keyValue, arrObj, dateKey, startDate, endDate)
+      }
+    });
+  }
+
+  generateKeys(key, arrObj) {
+    let flags = [], cats = [], l = arrObj.length, i;
+    for (i=0; i<l; i++) {
+      if (flags[arrObj[i][key]]) continue;
+      flags[arrObj[i][key]] = true;
+      cats.push(arrObj[i][key]);
+    }
+    return cats;
+  }
+
+  generateVals(keyCat, valCat, keyValue, arrObj, dateKey, startDate, endDate) {
+    let values = [], l = arrObj.length, i;
+    for (i=0; i<l; i++) {
+      let arrObjTime = new Date(arrObj[i][dateKey]).getTime();
+      let startDateTime = new Date(startDate).getTime();
+      let endDateTime = new Date(endDate).getTime();
+      if ((arrObj[i][keyCat] == valCat) && (startDateTime <= arrObjTime) && (arrObjTime <= endDateTime)) {
+        values.push(
+          {
+            x: arrObjTime,
+            y: arrObj[i][keyValue]
+          }
+        )
+      }
+    }
+    return values;
+  }
+
   ngOnInit() {
-    console.log('ong ngOnInit');
     this.options = this.getOptions.loadOptionLine("Tahun", "Jiwa");
+  }
+
+  onChange() {
+    this.data = this.generateData('rincian', 'jumlah', this.origData, 'tahun', this.dari, this.sampai);
+    this.nvD3.chart.update();
   }
 
 }
