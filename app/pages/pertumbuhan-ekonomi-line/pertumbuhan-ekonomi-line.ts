f608f508364601c87200d336ce980801645741cd
Habib Ridho <habibridho@live.com>
2016-07-15 18:48:31 +0700

    filter pertumbuhan ekonomi line done

--- app/pages/pertumbuhan-ekonomi-line/pertumbuhan-ekonomi-line.ts
+++ app/pages/pertumbuhan-ekonomi-line/pertumbuhan-ekonomi-line.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Injectable} from '@angular/core';
+import {Component, OnInit, Injectable, ViewChild} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {DataService} from '../../providers/data-service/data-service';
 import {nvD3} from '../../ng2-nvd3';
@@ -18,36 +18,46 @@ declare let d3: any;
   templateUrl: 'build/pages/pertumbuhan-ekonomi-line/pertumbuhan-ekonomi-line.html',
 })
 export class PertumbuhanEkonomiLinePage {
+  @ViewChild(nvD3) nvD3: nvD3;
   url = "https://api.kawaljakarta.org/v1/pertumbuhan-ekonomi/data&sortBy=tahun&order=asc";
   data;
   options;
+  origData;
   dateArr;
+  dari;
+  keys;
+  sampai;
 
   constructor(public http: Http, public nav: NavController, public dataService: DataService, public getOptions: Options) {
     console.log("on Constructor");
   }
 
   ngOnInit() {
-    console.log("onInit");
     this.dataService.load(this.url)
       .then(data => {
-        let keys = ['persen_tumbuh_jakarta','persen_tumbuh_nasional'];
-        this.data = this.genData(data, 'tahun', keys, this.genObject);
+        this.origData = data;
+        this.keys = ['persen_tumbuh_jakarta','persen_tumbuh_nasional'];
+        this.dateArr = this.genKeys('tahun', data);
+        this.dari = this.dateArr[0];
+        this.sampai = this.dateArr[this.dateArr.length-1];
+        let startTime = new Date(this.dari).getTime();
+        let endTime = new Date(this.sampai).getTime();
+        this.data = this.genData(data, 'tahun', this.keys, this.genObject, startTime, endTime);
         this.data[0].key = "Jakarta";
         this.data[1].key = "Nasional";
         this.options = this.getOptions.loadOptionLine("","%",null,-22, 40);
-        this.dateArr = this.genKeys('tahun', data);
+        console.log(data);
         console.log(this.data);
-        // console.log(this.dateArr);
       });
     }
 
-    genObject(data, key, dateKey) {
+    genObject(data, key, dateKey, startTime, endTime) {
       let values = [], l = data.length, i;
       for (i=0; i<l; i++) {
-        if (data[i][key]) {
+        let objTime = new Date(data[i][dateKey]).getTime();
+        if (data[i][key] && startTime <= objTime && objTime <= endTime) {
           values.push({
-            x: new Date(data[i][dateKey]).getTime(),
+            x: objTime,
             y: data[i][key]
           })
         }
@@ -58,10 +68,10 @@ export class PertumbuhanEkonomiLinePage {
       }
     }
 
-    genData(data, dateKey, arrKey, genObject) {
+    genData(data, dateKey, arrKey, genObject, startTime, endTime) {
       let arrObj = [];
       for (let key of arrKey) {
-        arrObj.push(genObject(data, key, 'tahun'))
+        arrObj.push(genObject(data, key, 'tahun', startTime, endTime))
       }
       return arrObj;
     }
@@ -74,6 +84,34 @@ export class PertumbuhanEkonomiLinePage {
         cats.push(arrObj[i][key]);
       }
       return cats;
+    }
+
+    onChangeStartYear() {
+      let startDate = new Date(this.dari);
+      let endDate = new Date(this.sampai);
+      if (startDate > endDate) {
+        let startYear = startDate.getFullYear();
+        endDate.setFullYear(startYear);
+        this.sampai = endDate.toISOString();
+      }
+      let startTime = startDate.getTime();
+      let endTime = endDate.getTime();
+      this.data = this.genData(this.origData, 'tahun', this.keys, this.genObject, startTime, endTime);
+      this.nvD3.chart.update();
+    }
+
+    onChangeEndYear() {
+      let startDate = new Date(this.dari);
+      let endDate = new Date(this.sampai);
+      if (endDate < startDate) {
+        let endYear = endDate.getFullYear();
+        startDate.setFullYear(endYear);
+        this.dari = startDate.toISOString();
+      }
+      let startTime = startDate.getTime();
+      let endTime = endDate.getTime();
+      this.data = this.genData(this.origData, 'tahun', this.keys, this.genObject, startTime, endTime);
+      this.nvD3.chart.update();
     }
 
 }
