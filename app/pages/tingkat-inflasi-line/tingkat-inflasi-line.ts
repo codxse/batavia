0853e6eaa7991e6d8b0fa6f387b795254a1d9e25
Nadiar AS <codxse@gmail.com>
2016-07-15 15:00:37 +0700

    tingkat inflasi line, yet filter still not implemented

--- app/pages/tingkat-inflasi-line/tingkat-inflasi-line.ts
+++ app/pages/tingkat-inflasi-line/tingkat-inflasi-line.ts
@@ -1,15 +1,79 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, Injectable} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {DataService} from '../../providers/data-service/data-service';
+import {nvD3} from '../../ng2-nvd3';
+import {Options} from '../../providers/options';
+import {Http} from '@angular/http';
+declare let d3: any;
 
 /*
-  Generated class for the TingkatInflasiLinePage page.
+  Generated class for the PertumbuhanEkonomiLinePage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
+  directives: [nvD3],
+  providers: [DataService, Options],
   templateUrl: 'build/pages/tingkat-inflasi-line/tingkat-inflasi-line.html',
 })
 export class TingkatInflasiLinePage {
-  constructor(public nav: NavController) {}
+  url = "https://api.kawaljakarta.org/v1/tingkat-inflasi/data&sortBy=tahun&order=asc";
+  data;
+  options;
+  dateArr;
+
+  constructor(public http: Http, public nav: NavController, public dataService: DataService, public getOptions: Options) {
+    console.log("on Constructor");
+  }
+
+  ngOnInit() {
+    console.log("onInit");
+    this.dataService.load(this.url)
+      .then(data => {
+        let keys = ['inflasi_jakarta','inflasi_nasional'];
+        this.data = this.genData(data, 'tahun', keys, this.genObject);
+        this.data[0].key = "Jakarta";
+        this.data[1].key = "Nasional";
+        this.options = this.getOptions.loadOptionLine("","Inflasi (%)",null,-22, 40);
+        this.dateArr = this.genKeys('tahun', data);
+        console.log(this.data);
+        // console.log(this.dateArr);
+      });
+    }
+
+    genObject(data, key, dateKey) {
+      let values = [], l = data.length, i;
+      for (i=0; i<l; i++) {
+        if (data[i][key]) {
+          values.push({
+            x: new Date(data[i][dateKey]).getTime(),
+            y: data[i][key]
+          })
+        }
+      }
+      return {
+        key: key,
+        values: values
+      }
+    }
+
+    genData(data, dateKey, arrKey, genObject) {
+      let arrObj = [];
+      for (let key of arrKey) {
+        arrObj.push(genObject(data, key, 'tahun'))
+      }
+      return arrObj;
+    }
+
+    genKeys(key, arrObj) {
+      let flags = [], cats = [], l = arrObj.length, i;
+      for (i=0; i<l; i++) {
+        if (flags[arrObj[i][key]]) continue;
+        flags[arrObj[i][key]] = true;
+        cats.push(arrObj[i][key]);
+      }
+      return cats;
+    }
+
 }
