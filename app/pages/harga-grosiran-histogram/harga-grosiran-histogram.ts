3aae65095fac39b03c863e640a590f41f66906c4
Nadiar AS <codxse@gmail.com>
2016-07-16 19:51:03 +0700

    histogram done

--- app/pages/harga-grosiran-histogram/harga-grosiran-histogram.ts
+++ app/pages/harga-grosiran-histogram/harga-grosiran-histogram.ts
@@ -1,15 +1,62 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {DataService} from '../../providers/data-service/data-service';
+import {nvD3} from '../../ng2-nvd3';
+import {Options} from '../../providers/options';
 
 /*
-  Generated class for the HargaGrosiranHistogramPage page.
+  Generated class for the KomponenInflasiPiePage page.
 
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
 @Component({
+  directives: [nvD3],
+  providers: [DataService, Options],
   templateUrl: 'build/pages/harga-grosiran-histogram/harga-grosiran-histogram.html',
 })
 export class HargaGrosiranHistogramPage {
-  constructor(public nav: NavController) {}
+  private url: String;
+  private data: Array<any>;
+  private yearArr: Array<Number>;
+  private options: any;
+
+  constructor(private nav: NavController, private dataService: DataService, private loadOptions: Options) {
+
+  }
+
+  private ngOnInit(): void {
+    this.url = "https://api.kawaljakarta.org/v1/perkembangan-harga-grosir/histogram";
+    this.loadData(this.url);
+  }
+
+
+  private loadData(url): void {
+    this.dataService.load(this.url)
+      .then(data => {
+        this.data = this.handleData(data);
+        console.log(this.data);
+        this.options = this.loadOptions.loadOptionHistogram("frekuensi", 45, 40, 75);
+      });
+  }
+
+  private handleData(datum): Array<any> {
+    let dataArr = [];
+    for (let data of datum) {
+      dataArr.push({
+        label: (function(d) {
+                  if (d.length > 20) {
+                    return d.substring(0, 20) + '...';
+                  }
+                    return d;
+                })(data.kelas_interval),
+        value: data.frekuensi
+      });
+    }
+    return [{
+      key: "Cumulative Return",
+      values: dataArr
+    }];
+  }
+
 }
