330f3f0577290a80a2c0468ef91b9805bb034814
Habib Ridho <habibridho@live.com>
2016-07-16 22:20:56 +0700

    filter nilai impor line

--- app/pages/nilai-impor-produk-line/nilai-impor-produk-line.ts
+++ app/pages/nilai-impor-produk-line/nilai-impor-produk-line.ts
@@ -21,12 +21,14 @@ export class NilaiImporProdukLinePage {
   private url: String;
   private options: any;
   private data: Array<any>;
-  private dateArr: Array<String>;
-  private yearArr: Array<String>;
-  private monthArr: Array<String>;
-  private monthNames: Array<String>
-  private dari;
-  private sampai;
+  private dateArr: Array<string>;
+  private yearArr: Array<any>;
+  private monthArr: Array<any>;
+  private monthNames: Array<string>
+  private startMonth;
+  private startYear;
+  private endMonth;
+  private endYear;
   private origData;
 
   constructor(public nav: NavController, public dataService: DataService, public getOptions: Options) {
@@ -35,8 +37,9 @@ export class NilaiImporProdukLinePage {
 
   private ngOnInit(): void {
     this.url = "https://api.kawaljakarta.org/v1/nilai-impor-produk-menurut-golongan";
+    this.monthArr = [0,1,2,3,4,5,6,7,8,9,10,11];
     this.monthNames = ["Januari", "Februari", "Maret", "April",
-                      "Mei", "Juni", "Juli", "Augustus", "September",
+                      "Mei", "Juni", "Juli", "Agustus", "September",
                       "Oktober", "November", "Desember"];
     this.loadData(this.url);
     this.options = this.getOptions.loadOptionLine("", "Juta USD", "%b %Y", null, -5, 55);
@@ -46,27 +49,27 @@ export class NilaiImporProdukLinePage {
     console.log('on loadData')
     this.dataService.load(url)
     .then(data => {
-      // this.origData = data;
+      this.origData = data;
+
       this.dateArr = this.generateKeys('bulan', data);
       this.yearArr = this.getYearArr(this.dateArr);
-      this.monthArr = this.getMonthArr(this.dateArr);
-      console.log(this.yearArr);
-      console.log(this.monthArr);
-      // let dateArrLength = this.dateArr.length;
-      // if (dateArrLength <= 5) {
-      //   this.dari = this.dateArr[0];
-      // } else {
-      //   this.dari = this.dateArr[dateArrLength-6];
-      // }
-      // this.sampai = this.dateArr[dateArrLength-1];
-      //
-      let startTime = new Date("2013-05-01").getTime();
-      let endTime = new Date("2014-10-01").getTime();
+
+      let dateArrLastIndex = this.dateArr.length-1;
+      let endLatestDate = new Date(this.dateArr[dateArrLastIndex]);
+
+      if (endLatestDate.getMonth() < 5) this.startMonth = endLatestDate.getMonth()+7;
+      else this.startMonth = endLatestDate.getMonth()-5;
+      this.startYear = endLatestDate.getFullYear();
+      this.endMonth = endLatestDate.getMonth();
+      this.endYear = endLatestDate.getFullYear();
+
+      let startLatestDate = new Date(this.startYear,this.startMonth);
+
+      let startTime = startLatestDate.getTime();
+      let endTime = endLatestDate.getTime();
 
       this.data = this.generateData('golongan', 'nilai_fob', data, 'bulan', startTime, endTime);
-      // this.data[0].key = "Ekspor Melalui Jakarta";
-      // this.data[1].key = "Ekspor Produk Jakarta";
-      // this.data[2].key = "Impor Melalui Jakarta";
+      console.log(this.data);
     });
   }
 
@@ -155,38 +158,74 @@ export class NilaiImporProdukLinePage {
     return values;
   }
 
-  onChangeStartYear() {
-    let startDate = new Date(this.dari);
-    let endDate = new Date(this.sampai);
-    if (startDate > endDate) {
-      let startYear = startDate.getFullYear();
-      endDate.setFullYear(startYear);
-      this.sampai = endDate.toISOString();
+  onChange(ngModel) {
+    let startDate = new Date(this.startYear, this.startMonth);
+    let endDate = new Date(this.endYear, this.endMonth);
+
+    if (ngModel == 'start') {
+      if (startDate > endDate) {
+        endDate = startDate;
+        this.endMonth = this.startMonth;
+        this.endYear = this.startYear;
+      }
+    } else {
+      if (endDate < startDate) {
+        startDate = endDate;
+        this.startMonth = this.endMonth;
+        this.startYear = this.endYear;
+      }
     }
+
+    switch (ngModel) {
+      case 'startMonth' :
+        break;
+      case 'endMonth' :
+        break;
+      case 'startYear' :
+        break;
+      case 'endYear' :
+        break;
+    }
+
     let startTime = startDate.getTime();
     let endTime = endDate.getTime();
-    this.data = this.generateData('atribut', 'juta_usd', this.origData, 'tahun', startTime, endTime);
-    this.data[0].key = "Ekspor Melalui Jakarta";
-    this.data[1].key = "Ekspor Produk Jakarta";
-    this.data[2].key = "Impor Melalui Jakarta";
-    this.nvD3.chart.update();
+
+    this.data = this.generateData('golongan', 'nilai_fob', this.origData, 'bulan', startTime, endTime);
+    console.log(this.data);
   }
 
-  onChangeEndYear() {
-    let startDate = new Date(this.dari);
-    let endDate = new Date(this.sampai);
-    if (endDate < startDate) {
-      let endYear = endDate.getFullYear();
-      startDate.setFullYear(endYear);
-      this.dari = startDate.toISOString();
-    }
-    let startTime = startDate.getTime();
-    let endTime = endDate.getTime();
-    this.data = this.generateData('atribut', 'juta_usd', this.origData, 'tahun', startTime, endTime);
-    this.data[0].key = "Ekspor Melalui Jakarta";
-    this.data[1].key = "Ekspor Produk Jakarta";
-    this.data[2].key = "Impor Melalui Jakarta";
-    this.nvD3.chart.update();
-  }
+  // onChangeStartYear() {
+  //   let startDate = new Date(this.dari);
+  //   let endDate = new Date(this.sampai);
+  //   if (startDate > endDate) {
+  //     let startYear = startDate.getFullYear();
+  //     endDate.setFullYear(startYear);
+  //     this.sampai = endDate.toISOString();
+  //   }
+  //   let startTime = startDate.getTime();
+  //   let endTime = endDate.getTime();
+  //   this.data = this.generateData('atribut', 'juta_usd', this.origData, 'tahun', startTime, endTime);
+  //   this.data[0].key = "Ekspor Melalui Jakarta";
+  //   this.data[1].key = "Ekspor Produk Jakarta";
+  //   this.data[2].key = "Impor Melalui Jakarta";
+  //   this.nvD3.chart.update();
+  // }
+  //
+  // onChangeEndYear() {
+  //   let startDate = new Date(this.dari);
+  //   let endDate = new Date(this.sampai);
+  //   if (endDate < startDate) {
+  //     let endYear = endDate.getFullYear();
+  //     startDate.setFullYear(endYear);
+  //     this.dari = startDate.toISOString();
+  //   }
+  //   let startTime = startDate.getTime();
+  //   let endTime = endDate.getTime();
+  //   this.data = this.generateData('atribut', 'juta_usd', this.origData, 'tahun', startTime, endTime);
+  //   this.data[0].key = "Ekspor Melalui Jakarta";
+  //   this.data[1].key = "Ekspor Produk Jakarta";
+  //   this.data[2].key = "Impor Melalui Jakarta";
+  //   this.nvD3.chart.update();
+  // }
 
 }
